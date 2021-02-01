1a5f258297f1cb335b087d46bc74fab7cef39ade
ReamLasry <ream794613@gmail.com>
2021-02-01 19:17:04 +0200

    JS update(RENDER_FUNCTION

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 console.log('Starting up');
+'use strict';
+
+projectsRender();
+
+function projectsRender() {
+
+    var strHTMLs = projects.map(function(project) {
+        `<div class="col-md-4 col-sm-6 portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${project.id}">
+                <div class="portfolio-hover">
+                    <div class="portfolio-hover-content">
+                        <i class="fa fa-plus fa-3x"></i>
+                    </div>
+                </div>
+                <img class="img-fluid" src="${project.imgURL}" alt="">
+            </a>
+            <div class="portfolio-caption">
+                <h4>${project.name}</h4>
+                <p class="text-muted">${project.title}</p>
+            </div>
+        </div>`
+    }).join('');
+
+
+    var $elProjects = $('.projects');
+    $elProjects.html(strHTMLs);
+
+}
